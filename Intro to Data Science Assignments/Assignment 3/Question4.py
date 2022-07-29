8c9f81837e6d9a6cfea8ea5a60a3980b4d8426bb
Joel Quek <103588217+joel-quek@users.noreply.github.com>
2022-07-29 18:09:41 +0800

    dgsdgsd

---
+++ Intro to Data Science Assignments/Assignment 3/Question4.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import re    
+
+def answer_one():  
+    # assets/Energy Indicators.xls
+    energy = pd.read_excel('Energy Indicators.xls', header= None, skiprows=18, skipfooter=38) # remove header and footer
+    energy.replace('...', np.NaN, inplace=True) # replace .. with np.Nan
+    energy.drop(energy.columns[[0,1]], axis=1, inplace=True) # remove first two columns
+    energy.columns= ['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable'] # rename columns
+    energy['Energy Supply']=energy['Energy Supply'].apply(lambda x: x*1000000) # change from petajoules to gigajoules
+
+    energy['Country'] = energy['Country'].str.replace(r"[0-9]+$","", regex=True) # remove superscripts
+    energy['Country'] = energy['Country'].str.replace(r" \(.*\)$","", regex=True)  # remove parentheses
+
+
+    energy['Country'] = energy['Country'].replace({'Republic of Korea' : 'South Korea',
+                                                'United States of America' : 'United States',
+                                                'United Kingdom of Great Britain and Northern Ireland':'United Kingdom',
+                                                'China, Hong Kong Special Administrative Region':'Hong Kong'}) # replace required country names
+    # -------------------------------------------------------------------------------------------------------
+    gdp = pd.read_csv('GDP.csv', skiprows=4)
+    gdp['Country Name'] = gdp['Country Name'].replace({'Korea, Rep.': 'South Korea', 
+                                                        'Iran, Islamic Rep.': 'Iran', 
+                                                        'Hong Kong SAR, China' : 'Hong Kong'}) 
+    gdp.rename(columns={'Country Name': 'Country'}, inplace=True)
+    gdp = gdp.loc[:,['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015',"Country"]]
+    # -------------------------------------------------------------------------------------------------------
+    ScimEn = pd.read_excel('scimagojr-3.xlsx') # your file cannot be opened at the same time you are trying to run the code
+    # -------------------------------------------------------------------------------------------------------
+    # scimagojir-energy-gdp2
+    join1 = pd.merge(ScimEn,energy,how="inner",left_on="Country",right_on="Country")
+    join1 = join1[join1["Rank"]<=15]
+
+    join2 = pd.merge(join1, gdp, how="inner", left_on='Country', right_on='Country')
+    join2 = join2.set_index('Country')
+    return join2
+# -------------------------------------------------------------------------------------------------------
+def answer_four():
+    data_info = answer_one()
+    data_info['avgGDP'] = data_info[['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']].mean(axis=1)
+    data_info.sort_values(['avgGDP'], ascending=False, inplace=True)
+    # gdp6 = data_info.index[5]
+    diff = data_info.iloc[5]['2015'] - data_info.iloc[5]['2006']
+    return diff 
+
+print(answer_four())
