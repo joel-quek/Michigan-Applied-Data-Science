c2c58e321e30431caf159b785b1cbc8a9638b318
Joel Quek <103588217+joel-quek@users.noreply.github.com>
2022-07-30 18:39:32 +0800

    xcbcxbcxbc

--- Group By/group_by.py
+++ Group By/group_by.py
@@ -1,1 +1,12 @@
+# How to use groupby() to group categories in a pandas DataFrame
+# talks about multi level indexing also
 https://www.youtube.com/watch?v=ipoSjrN0oh0
+
+# Loop / Iterate over Pandas Dataframes
+https://www.youtube.com/watch?v=CG3EV7UBELA&list=PLjNQtX45f0dRONMZZKkCzn_EjdzUXc8Rx
+
+# LOC and ILOC
+https://www.statology.org/pandas-loc-vs-iloc/#:~:text=When%20it%20comes%20to%20selecting,columns%20at%20specific%20integer%20positions
+
+# Pandas Multi Level Indexing
+https://www.youtube.com/watch?v=tcRGa2soc-c
